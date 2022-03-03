bcfd1498c5ea39941038d1c012ce49d4dd31d049
Emmanuel <e.m.social35@gmail.com>
2022-03-03 14:47:25 -0600

    added robot class

---
+++ robot.py
@@ -0,0 +1,15 @@
+
+
+
+
+from weapon import Weapon
+
+
+class Robot:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.health = 500
+        self.weapon = Weapon
+
+    def attack(self, dinosaur): #void
+        pass
