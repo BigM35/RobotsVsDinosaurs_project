9d5c6c2699e4e6bbc2d7889c7c56018c658ef178
Emmanuel <e.m.social35@gmail.com>
2022-03-03 22:28:28 -0600

    added logic to create and give weapons to the robot class

--- robot.py
+++ robot.py
@@ -10,3 +10,13 @@ class Robot:
 
     def attack(self, dinosaur): #void
         pass
+
+    def give_weapon(self):
+        user_choice = input("Choose your ability!\n 1. Excalibur\n2. Force Cascade\n3. Meteor Cannon")
+        if  user_choice == 1:
+            self.weapon = Weapon("Excalibur", 75)
+        elif user_choice == 2:    
+            self.weapon = Weapon(" Force Cascade", 50)
+        elif user_choice == 3:    
+            self.weapon = Weapon("Meteor Cannon", 100)
+
